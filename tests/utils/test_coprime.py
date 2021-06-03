615c679e5fbfb0bcdec8ed812a072651a035e960
Reid Hochstedler <reidhoch@gmail.com>
2021-06-03 16:30:25 -0400

    LCM is simple enough to not worry about specific Python versions

--- tests/utils/test_coprime.py
+++ tests/utils/test_coprime.py
@@ -1,21 +1,16 @@
 # coding=utf-8
-import sys
-from functools import reduce
 from math import gcd
 
 from hypothesis import given
 from hypothesis.strategies import integers
 from oeis.utils import coprime
 
-if sys.version_info.major >= 3 and sys.version_info.minor >= 9:
-    from math import lcm
-else:
 
-    def lcm(*integers) -> int:
-        return reduce(lambda x, y: (x * y) / gcd(x, y), integers, 1)
+def lcm(x: int, y: int) -> int:
+    return (x * y) // gcd(x, y)
 
 
-@given(a=integers(min_value=2, max_value=100), b=integers(min_value=2, max_value=100))
+@given(a=integers(), b=integers())
 def test_lcm_property(a: int, b: int) -> None:
     if coprime(a, b):
         assert lcm(a, b) == a * b
