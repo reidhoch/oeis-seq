0e18b2c02d356ad6ceb399f2188bcecef02729c5
Reid Hochstedler <reidhoch@gmail.com>
2021-06-03 16:00:27 -0400

    Python 3.8 does not have math.lcm

--- tests/utils/test_coprime.py
+++ tests/utils/test_coprime.py
@@ -1,9 +1,18 @@
 # coding=utf-8
+import sys
+from functools import reduce
+from math import gcd
+
 from hypothesis import given
 from hypothesis.strategies import integers
-
 from oeis.utils import coprime
-from math import lcm
+
+if sys.version_info.major >= 3 and sys.version_info.minor >= 9:
+    from math import lcm
+else:
+
+    def lcm(*integers) -> int:
+        return reduce(lambda x, y: (x * y) / gcd(x, y), integers, 1)
 
 
 @given(a=integers(min_value=2, max_value=100), b=integers(min_value=2, max_value=100))
